c34917d9114917867371dca9fa298a5ff6e88b12
Anaïs Trempe <antre462@ulaval.ca>
2026-03-17 09:25:16 -0400

    7

--- newton.py
+++ newton.py
@@ -7,7 +7,7 @@ def newton(beta_init, F, J, tol, nmax):
     Args:
         beta_init : point initial (vecteur colonne 3x1)
         F         : fonction vectorielle (vecteur colonne nx1) dont on cherche le zero. DEPEND DE BETA.
-        J         : matrice jacobienne de F (matrice nxn). DEPEND DE BETA.
+        J         : matrice jacobienne de F (matrice nx3). DEPEND DE BETA.
         tol       : tolerance pour determiner la convergence
         nmax      : nombre maximal d'iterations
 
@@ -15,30 +15,34 @@ def newton(beta_init, F, J, tol, nmax):
         beta      : vecteur des coefficients de la courbe de regression
     '''
 
-    beta = beta_init;
+    beta = beta_init.copy()
     assert beta.shape == (3,1), "beta_init doit être un vecteur colonne!"
-    n = 0;
-    res = norm(F(beta.flatten()));
-    dbeta = np.inf;
+    n = 0
+    res = norm(F(beta.flatten()))
+    dbeta = np.inf
 
-    while res > tol and norm(dbeta) > tol and n < nmax :
-        Jb = J(beta.flatten());
-        Fb = F(beta.flatten());
+    while res > tol and norm(dbeta) > tol and n < nmax:
+        Jb = J(beta.flatten())
+        Fb = F(beta.flatten())
         assert Fb.shape == (1000,1), "F doit retourner un vecteur colonne!"
 
         # Resolution du systeme pour calculer la correction
-        # Calcul de dbeta en utilisant approche (A) ou (B)
-        # Modifier ici !
+        # Approche (A) : equations normales
+        A = Jb
+        b = -Fb
 
-        #dbeta = ...
+        ATA = A.T @ A
+        ATb = A.T @ b
+
+        dbeta = solve(ATA, ATb)
 
         # Appliquer la correction
         assert dbeta.shape == (3,1), "dbeta doit être un vecteur colonne!"
-        beta += dbeta;
+        beta += dbeta
 
         # Calcul du residu au nouveau point
-        res = norm(F(beta.flatten()));
+        res = norm(F(beta.flatten()))
         print(f"Iteration {n} : ||dbeta|| = {norm(dbeta)}, ||F(beta)|| = {res}")
-        n += 1;
+        n += 1
 
     return beta
