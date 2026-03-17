52c54fe878a580587a3b1fc6e96ab265e2fc1cca
camille286 <caroy200@ulaval.ca>
2026-03-17 08:55:49 -0400

    4

--- Equipe190.py
+++ Equipe190.py
@@ -2,8 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 X = np.loadtxt("points.txt")
-plt.scatter(X[:,0], X[:,1], s=3, c='purple')
-
+plt.scatter(X[:,0], X[:,1], s=3, c='plum')
 plt.xlabel("x")
 plt.ylabel("f(x)")
 plt.title("Nuage de points")
