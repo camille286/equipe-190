6a535dd79ed8db3d5f1461bd07a045929181698c
camille286 <caroy200@ulaval.ca>
2026-03-17 07:15:39 -0400

    1

---
+++ Equipe190.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from time import time as time
+from reglinA import reglinA
+from reglinB import reglinB
+from newton import newton
+from regfreqA import regfreqA
+from regfreqB import regfreqB
