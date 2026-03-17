f3abc02214a14660b82d1571a8caaae5c2683228
camille286 <caroy200@ulaval.ca>
2026-03-17 09:03:14 -0400

    5

--- Equipe190.py
+++ Equipe190.py
@@ -1,5 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time as time
+from reglinA import reglinA
+from reglinB import reglinB
+from newton import newton
+
+from regfreqA import regfreqA
+from regfreqB import regfreqB
 
 X = np.loadtxt("points.txt")
 plt.scatter(X[:,0], X[:,1], s=3, c='plum')
