8acf625396476047655a8fc00a0da9625c689d7a
camille286 <caroy200@ulaval.ca>
2026-03-17 08:48:15 -0400

    3

--- Equipe190.py
+++ Equipe190.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from time import time as time
-from reglinA import reglinA
-from reglinB import reglinB
-from newton import newton
-from regfreqA import regfreqA
-from regfreqB import regfreqB
 
 X = np.loadtxt("points.txt")
-plt.scatter(X[:,0],X[:,1],s=3)
+plt.scatter(X[:,0], X[:,1], s=3, c='purple')
+
+plt.xlabel("x")
+plt.ylabel("f(x)")
+plt.title("Nuage de points")
+
+plt.grid()
+plt.show()
